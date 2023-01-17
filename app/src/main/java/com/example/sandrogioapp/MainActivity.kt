de3a8500d434189f4cb876adbdd4d2b89a29c44e
giorgi-kavtiashvili <115482579+giorgi-kavtiashvili@users.noreply.github.com>
2023-01-17 04:16:36 +0400

    ver dasrulebuli ragaca :D

--- app/src/main/java/com/example/sandrogioapp/MainActivity.kt
+++ app/src/main/java/com/example/sandrogioapp/MainActivity.kt
@@ -2,11 +2,46 @@ package com.example.sandrogioapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.lumaspotify.R
+import com.example.lumaspotify.databinding.ActivityMainBinding
+import com.example.sandrogioapp.fragments.HomeFragment
+import com.example.sandrogioapp.fragments.ImageUploadFragment
+import com.example.sandrogioapp.fragments.ProfileFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(HomeFragment())
+
+        binding.bottomNavigationView.setOnItemReselectedListener {
+            when(it.itemId){
+                R.id.homeFragment -> replaceFragment(HomeFragment())
+                R.id.profileFragment -> replaceFragment(ProfileFragment())
+                R.id.imageFragment -> replaceFragment(ImageUploadFragment())
+
+                else ->{
+
+                }
+            }
+
+            true
+
+        }
+
+
     }
+    private fun replaceFragment(fragment : Fragment){
+
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
+    }
+
 }
--- app/src/main/java/com/example/sandrogioapp/fragments/ImageUploadFragment.kt
+++ app/src/main/java/com/example/sandrogioapp/fragments/ImageUploadFragment.kt
@@ -1,7 +1,56 @@
 package com.example.sandrogioapp.fragments
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import com.example.lumaspotify.R
+import com.example.lumaspotify.databinding.FragmentImageuploadBinding
+
 
 class ImageUploadFragment : Fragment(R.layout.fragment_imageupload) {
+
+
+    private lateinit var binding: FragmentImageuploadBinding
+    private lateinit var imageView: ImageView
+
+    companion object{
+        val IMAGE_REQUEST_CODE = 100
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentImageuploadBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        ImageUploadActivityListeners()
+
+    }
+    private fun ImageUploadActivityListeners(){
+        binding.pickImageButton.setOnClickListener{
+            pickImageGallery()
+        }
+    }
+
+    private fun pickImageGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == IMAGE_REQUEST_CODE && resultCode == RESULT_OK) {
+            imageView.setImageURI(data?.data)
+        }
+    }
 }
